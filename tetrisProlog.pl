0cc5eb255206a3ba61763f7097277034f7325acc
Roberto Lillo <robertoxlp@hotmail.com>
2018-11-29 12:10:34 -0300

    Avance

--- tetrisProlog.pl
+++ tetrisProlog.pl
@@ -1,4 +1,25 @@
-%tetrisProlog
-entrenador(javo).
-entrenador(roers).
-entrenador(nico).
+% tetrisProlog
+% Laboratorio 2 Paradigmas de programación (2 - 2018).
+% Fecha de entrega: 7 de Diciembre.
+
+%Dominios
+
+
+%Predicados
+
+
+%Metas
+
+
+%Clausulas de Horn
+%Hechos
+
+
+%Reglas
+
+
+
+%------------------------------%
+
+%Papeo
+esPar(X) :- 0 is X mod 2.
